a865ab4c681802e28e2411787d74445854326d69
karnaankit <40463873+karnaankit@users.noreply.github.com>
2022-10-10 23:12:54 +0545

    cleanup

--- scrapper.py
+++ scrapper.py
@@ -13,19 +13,15 @@ def parser():
     try:
         with open('results.json', encoding='utf-8') as f:
             json_data = json.load(f)
-            title = json_data.get('data').get('title')
-            page = json_data.get('data').get('page') + 1
+            title = json_data.get('title')
+            page = json_data.get('page') + 1
     except:
-        json_data["data"] = params
-        json_data["data"]["articles"] = []
-        json.dump(json_data, open('results.json', 'w'), indent=4)
-        title = json_data.get('data').get('title')
-        page = json_data.get('data').get('page') + 1
+        json_data = params
+        json_data["articles"] = []
+        title = json_data.get('title')
+        page = json_data.get('page') + 1
     temp_page = page
-    err_count = 0
     while page <= temp_page + 2:
-        if err_count >= 1:
-            break
         try:
             params['page'] = page
             params['title'] = title
@@ -33,15 +29,14 @@ def parser():
             data = search.json()
 
             if search.status_code == 200 and data['status'] == 'success':
-                json_data["data"]['page'] = params['page']
+                json_data['page'] = params['page']
                 for i in data["data"]["items"]:
-                    json_data["data"]["articles"].append(i)
+                    json_data["articles"].extend(i)
                 json.dump(json_data, open('results.json', 'w'), indent=4)
                 print("Fetched page:", page, 'successfully')
                 page = page + 1
                 params['page'] = page
         except Exception as exc:
-            err_count += 1
             print(exc)
 
 
